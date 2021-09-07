efe5958b51a91129a53b6a8cb1b74a8c8beb4c57
Marat311 <marinavelitskaia@gmail.com>
2021-09-07 19:45:03 -0400

    AC 1 automated

--- src/test/java/com/libraryCt/LoginAsLibrarian.java
+++ src/test/java/com/libraryCt/LoginAsLibrarian.java
@@ -13,7 +13,11 @@ import java.util.List;
 public class LoginAsLibrarian {
     public static void main(String[] args) throws InterruptedException {
 
-        //Given librarian is on the loginPage
+        ArrayList<String> userNames = new ArrayList<>(Arrays.asList("librarian43@library", "librarian18@library"));
+
+        for (String name : userNames) {
+
+            //Given librarian is on the loginPage
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://library2.cybertekschool.com/login.html");
@@ -21,46 +25,62 @@ public class LoginAsLibrarian {
         //Then verify that the title is “Login - Library”
         String expectedTitle = "Login - Library";
         String actualTitle = driver.getTitle();
+            System.out.println("actualTitle = " + actualTitle);
 
-        if(expectedTitle.equals(actualTitle)){
-            System.out.printf("match");
-        }else{
-            System.out.printf("not match");
+            if (expectedTitle.equals(actualTitle)) {
+            System.out.println("Title verification Pass!");
+        } else {
+            System.out.println("Title verification Failed!!!");
             System.out.println("expectedTitle = " + expectedTitle);
             System.out.println("actualTitle = " + actualTitle);
         }
 
-        //When librarian enters valid email address and password
-          WebElement email = driver.findElement(By.id("inputEmail"));
-          email.sendKeys("librarian50@library");
 
-          Thread.sleep(3000);
+            //When librarian enters valid email address and password
+            WebElement email = driver.findElement(By.id("inputEmail"));
+            email.sendKeys(name);
+            System.out.println("Librarian userName = " + name);
+            //password
+            String password = "Sdet2022*";
+            driver.findElement(By.id("inputPassword")).sendKeys("Sdet2022*");
+            System.out.println("password = " + password);
 
-          WebElement password = driver.findElement(By.id("inputPassword"));
-          password.sendKeys("Sdet2022*");
+            //And librarian click sign in button
+           driver.findElement(By.cssSelector("#login-form > button")).click();
+            
+            //Then verify that there are 3 models on the page
+            Thread.sleep(3000);
 
-        Thread.sleep(3000);
-
-        //And librarian click sign in button
-        WebElement signInBtn = driver.findElement(new By.ByCssSelector("button"));
-        signInBtn.click();
-
-        Thread.sleep(3000);
-
-
-
-        //Then verify that there are 3 models on the page
-      List<String> expectedModules = new ArrayList<>(Arrays.asList("Dashboard" , "Users", "Books"));
-
-      //List<WebElement> actualModules = driver.findElements(By.id());
+            //
+            List<WebElement> titles = driver.findElements(By.xpath("//li[@class='nav-item']"));
+            System.out.println("titles.size() = " + titles.size());
 
 
+            if (driver.findElement(By.linkText("Dashboard")).getText().equals("Dashboard")) {
+                System.out.println("Title verification passed: " + driver.findElement(By.linkText("Dashboard")).getText());
+            } else {
+                System.out.println("Title verification failed");
+            }
+
+            if (driver.findElement(By.linkText("Users")).getText().equals("Users")) {
+                System.out.println("Title verification passed: " + driver.findElement(By.linkText("Users")).getText());
+            } else {
+                System.out.println("Title verification failed!!!");
+            }
+
+            if (driver.findElement(By.linkText("Books")).getText().equals("Books")) {
+                System.out.println("Title verification passed: " + driver.findElement(By.linkText("Books")).getText());
+            } else {
+                System.out.println("Title verification failed!!!");
+            }
+
+            driver.close();
+
+        }
 
 
-
-
-        //driver.quit();
     }
+
 }
 /*
 Given librarian is on the loginPage
